04dece3e2611c49d8ec1ce574d20488a78135535
WenerRodrigo <wener19.wr@gmail.com>
2022-11-10 22:35:39 -0300

    pesquisador de clima mundial

---
+++ js/script.js
@@ -0,0 +1,47 @@
+/*8cfaca83d473b234fd7080a5361036bb
+{
+    "coord": {
+      "lon": 10.99,
+      "lat": 44.34
+    },
+    "weather": [
+      {
+        "id": 804,
+        "main": "Clouds",
+        "description": "overcast clouds",
+        "icon": "04n"
+      }
+    ],
+    "base": "stations",
+    "main": {
+      "temp": 283.33,
+      "feels_like": 282.73,
+      "temp_min": 281.3,
+      "temp_max": 284.86,
+      "pressure": 1032,
+      "humidity": 89,
+      "sea_level": 1032,
+      "grnd_level": 944
+    },
+    "visibility": 10000,
+    "wind": {
+      "speed": 1.2,
+      "deg": 305,
+      "gust": 1.67
+    },
+    "clouds": {
+      "all": 100
+    },
+    "dt": 1668126900,
+    "sys": {
+      "type": 2,
+      "id": 2075663,
+      "country": "IT",
+      "sunrise": 1668146779,
+      "sunset": 1668182034
+    },
+    "timezone": 3600,
+    "id": 3163858,
+    "name": "Zocca",
+    "cod": 200
+  } */
